8e50b2e585b52cd932ab257243098b47562fc270
jamezjaz <jamezjaz@gmail.com>
2020-12-02 10:22:17 -0600

    docs: implements project select feature

--- src/index.js
+++ src/index.js
@@ -1,34 +1,51 @@
 import projectObj from './project';
 import todoObj from './todo';
 import {
-  mainContent, todos, todoPara, todoContents,
+  mainContent, todos, todoPara, todoContents, mySelect, option,
 } from './selectors';
 
 const allProjects = [];
 
+const selectOption = () => {
+  allProjects.forEach((proj) => {
+    option.innerHTML = proj.projectTitle;
+    mySelect.appendChild(option);
+  });
+};
+
 const showCurrentProject = (currentProject) => {
   allProjects.forEach((proj) => {
-    if (proj === currentProject) {
-      for (let i = 0; i < proj.todoList.length; i += 1) {
-        todoPara.innerHTML = proj.todoList[i].title;
-        // todoPara.innerHTML = proj.todoList[i].description;
-        alert('Project created successfully');
-        todos.appendChild(todoPara);
-        mainContent.appendChild(todos);
+    if (proj.projectTitle === currentProject) {
+      if (proj.todoList.length > 0) {
+        for (let i = 0; i < proj.todoList.length; i += 1) {
+          todoPara.innerHTML = proj.todoList[i].title;
+          todos.appendChild(todoPara);
+          mainContent.appendChild(todos);
+          alert('Project created successfully');
+        }
+      } else {
+        todoDetails.innerHTML = 'No Todos';
       }
-    } else {
-      alert('No Todos');
-    }
+    }  
   });
 };
 
 const defaults = () => {
   const defaultProject = projectObj('Default Project');
+  const secondProject = projectObj('second Project');
   const defaultTodo = todoObj('Task 1', 'Default', '21/11/2020', 'High');
+  const secondTodo = todoObj('Second Task', 'Description', '01/01/2021', 'Medium');
   defaultProject.todoList.push(defaultTodo);
+  secondProject.todoList.push(secondTodo);
   allProjects.push(defaultProject);
+  allProjects.push(secondProject);
   const currentProject = defaultProject;
-  showCurrentProject(currentProject);
+  showCurrentProject(currentProject.projectTitle);
+  selectOption();
 };
 
 defaults();
+
+mySelect.onchange = () => {
+  showCurrentProject(mySelect.value);
+};
--- src/selectors.js
+++ src/selectors.js
@@ -1,10 +1,11 @@
 const mainContent = document.getElementById('content');
-
 const todos = document.createElement('div');
 const todoPara = document.createElement('p');
+const mySelect = document.querySelector('#mySelect');
+const option = document.createElement('option');
 
 const todoContents = todoPara.innerHTML;
 
 export {
-  mainContent, todos, todoPara, todoContents,
+  mainContent, todos, todoPara, todoContents, mySelect, option,
 };
