baa3bf449357ddbc2fa9eba4c5a218c215b6605c
jamezjaz <jamezjaz@gmail.com>
2020-12-08 06:15:19 -0600

    docs: add tests for createTodoObj

---
+++ tests/createTodo.js
@@ -0,0 +1,28 @@
+const TodoObj = (title, description, dueDate, priority) => ({
+  title, description, dueDate, priority,
+});
+
+const todoForm = {
+  title: {
+    value: 'Shopping',
+  },
+  description: {
+    value: 'Groceries and toiletries',
+  },
+  dueDate: {
+    value: 22-12-2020,
+  },
+  priority: {
+    value: 'Medium',
+  },
+};
+
+const createTodo = () => {
+  const title = todoForm.title.value;
+  const description = todoForm.description.value;
+  const dueDate = todoForm.dueDate.value;
+  const priority = todoForm.priority.value;
+  return TodoObj(title, description, dueDate, priority);
+};
+
+module.exports = createTodo;
